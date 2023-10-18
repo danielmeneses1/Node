4cabe4f473fa4cbc397fec1725b82accde2153ee
daniel <danieljijoca@outlook.com>
2023-10-18 11:15:19 -0300

    18/10

---
+++ 01-fundamentos-nodeJS/fake-Upload-to-http-Stream.js
@@ -0,0 +1,24 @@
+import { Readable } from "stream";
+const fetch = require('node-fetch');
+
+
+class OneToHundredStream extends Readable{
+    index = 1
+    _read(){
+        const i = this.index++
+        setTimeout(() => {
+            if(i > 100){
+                this.push(null) 
+            }else{
+            const buf = Buffer.from(String(i))
+
+            this.push(buf)
+            }
+        }, 1000);
+}
+}
+
+fetch('http://localhost:3334', {
+    method: 'POST',
+    body: new OneToHundredStream()
+})
